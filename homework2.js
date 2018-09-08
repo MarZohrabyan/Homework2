bd49a99862a184a70e046aa6b39e5c9d4e4f546b
MarZohrabyan <maro_zohrabyan@edu .aua.am>
2018-09-08 13:08:14 +0400

    My First Homework

---
+++ homework2.js
@@ -0,0 +1,15 @@
+const a=12;
+const b=11;
+const c=23;
+  alert('Push the button OK');
+  alert("Welcome "+ (a+b)*c + " times");
+if(a===12 && c===11){
+   console.log('false');
+} else if(a===12 || b===22){
+   console.log('true');
+} 
+if(a+b === c){
+	console.log('Two sides are equal');
+} else{
+	console.log('You are wrong');
+}
